69b77979ae53001cf58537978512c74942d13fbc
luckyos-code <luckyos-code@users.noreply.github.com>
2020-02-05 19:00:17 +0100

    Add classes EntryType and Field, Parse data constraints

--- ShacShifter/BibLaTeXDefParser.py
+++ ShacShifter/BibLaTeXDefParser.py
@@ -8,10 +8,8 @@ class BibLaTeXDefParser:
     logger = logging.getLogger('ShacShifter.BibLaTeXDefParser')
 
     def __init__(self):
-        # Future node shapes
+        # Future shapes
         self.entryTypes = {}
-        # Future property shapes
-        self.fields = {}
 
     def parseDef(self, inputFilePath):
         """Parse the BibLaTeX Specification.
@@ -21,71 +19,178 @@ class BibLaTeXDefParser:
         """
         with open(inputFilePath, 'r') as file:
             content = file.read()
-            reg_match = _RegExLib(content)
+            reg_match = _RegExLib(content.replace(' ',''))
 
+            fieldsDict = {}
+            
             if reg_match.constants:
+                # not (yet) relevant
                 pass
 
             if reg_match.entryTypes:
+                print(len(reg_match.entryTypes))
                 for declaration in reg_match.entryTypes:
                     if declaration[0] == '':
-                        for entry in [element.strip('\n ') for element in declaration[1].split(',')]:
-                            self.entryTypes[entry] = {'skipout': False, 'fields': set()}
+                        for entry in [element.strip('\n') for element in declaration[1].split(',')]:
+                            self.entryTypes[entry] = EntryType(name=entry, skipout=False)
                     if declaration[0] == '[skipout]':
-                        for entry in [element.strip('\n ') for element in declaration[1].split(',')]:
-                            self.entryTypes[entry] = {'skipout': True, 'fields': set()}
+                        for entry in [element.strip('\n') for element in declaration[1].split(',')]:
+                            self.entryTypes[entry] = EntryType(name=entry, skipout=True)
 
             if reg_match.fields:
+                print(len(reg_match.fields))
                 # missing options: format, nullok, skipout, label
-                fields = set()
                 for declaration in reg_match.fields:
-                    options = [element.split('=') for element in declaration[0].strip('[]').split(', ')]
+                    options = [element.split('=') for element in declaration[0].strip('[]').split(',')]
                     for entry in [element.strip() for element in declaration[1].split(',')]:
                         for option in options:
                             if option[0] == 'type':
                                 fieldType = option[1]
                             if option[0] == 'datatype':
                                 dataType = option[1]
-                        self.fields[entry] = {'type': fieldType, 'datatype': dataType}
+                        fieldsDict[entry] = Field(name=entry, type=fieldType, dataType=dataType)
 
             if reg_match.entryFields:
+                print(len(reg_match.entryFields))
                 for declaration in reg_match.entryFields:
                     if declaration[0] == '':
-                        fields = [element.strip('\n ') for element in declaration[1].split(',')]
+                        # for all entryTypes
+                        fields = [element.strip('\n') for element in declaration[1].split(',')]
+                        for num, field in enumerate(fields):
+                            if field not in fieldsDict:
+                                fieldsDict[field] = Field(name=field, type=False, dataType=False)
+                                fields[num] = fieldsDict[field]
+                            else:
+                                fields[num] = fieldsDict[field]
                         for key, value in self.entryTypes.items():
-                            value['fields'].update(fields)
+                            self.entryTypes[key].fields.extend(fields)
                     else:
+                        # for specific entryTypes
                         types = declaration[0].strip('[]').split(',')
-                        fields = [element.strip('\n ') for element in declaration[1].split(',')]
+                        fields = [element.strip('\n') for element in declaration[1].split(',')]
+                        for num, field in enumerate(fields):
+                            if field not in fieldsDict:
+                                fieldsDict[field] = Field(name=field, type=False, dataType=False)
+                                fields[num] = fieldsDict[field]
+                            else:
+                                fields[num] = fieldsDict[field]
                         for key in types:
-                            self.entryTypes[key]['fields'].update(fields)
+                            self.entryTypes[key].fields.extend(fields)
 
             if reg_match.multiscriptEntryFields:
-                # print(reg_match.multiscriptEntryFields)
-                pass
+                print(len(reg_match.multiscriptEntryFields))
+                # not (yet) relevant
+                for declaration in reg_match.multiscriptEntryFields:
+                    if declaration[0] == '':
+                        # for all entryTypes
+                        for entry in [element.strip('\n') for element in declaration[1].split(',')]:
+                            pass
+                    else:
+                        # for specific entryTypes
+                        types = declaration[0].strip('[]').split(',')
+                        fields = [element.strip('\n') for element in declaration[1].split(',')]
+                        for key in types:
+                            pass
 
             if reg_match.constraints:
-                # print(reg_match.constraints)
-                # print(re.findall('(.*?)', reg_match.constraints[0]))
+                regConstraint = r'\\constraint(\[.*?\]){(.*?)^}'
+
+                for declaration in reg_match.constraints:
+                    if declaration[0] == '':
+                        # for all entryTypes (add to fields dict)
+                        constraints = re.findall(regConstraint, declaration[1], re.DOTALL | re.MULTILINE)
+                        if constraints:
+                            self.parseConstraints(constraints, types=False)
+                    else:
+                        # for specific entryTypes (add to entryType dict)
+                        types = declaration[0].strip('[]').split(',')
+                        constraints = re.findall(regConstraint, declaration[1], re.DOTALL | re.MULTILINE)
+                        if constraints:
+                            self.parseConstraints(constraints, types)
+
+        return self.entryTypes
+
+    def parseConstraints(self, constraints, types=False):
+        regOR = r''
+        regXOR = r''
+        regField = r'\\constraintfield{(.*?)}'
+
+        for constraint in constraints:
+            options = [element.split('=') for element in constraint[0].strip('[]').split(',')]
+            constraint = constraint[1].strip('\n')
+            dataType = rangemin = rangemax = pattern = False
+            for option in options:
+                if option[0] == 'type':
+                    constraintType = option[1]
+                if option[0] == 'datatype':
+                    dataType = option[1]
+                if option[0] == 'rangemin':
+                    rangemin = option[1]
+                if option[0] == 'rangemax':
+                    rangemax = option[1]
+                if option[0] == 'pattern':
+                    pattern = option[1]
+            if constraintType == 'mandatory':
+                # all mandatory
+                # or
+                # xor
+                pass
+            elif constraintType == 'data':
+                constraintFields = re.findall(regField, constraint, re.DOTALL | re.MULTILINE)
+                if types:
+                    for key in types:
+                        for num, field in enumerate(self.entryTypes[key].fields):
+                            if field.name in constraintFields:
+                                self.entryTypes[key].fields[num].contraints = {
+                                    'dataype': dataType,
+                                    'rangemin': rangemin,
+                                    'rangemax': rangemax,
+                                    'pattern': pattern
+                                }
+                else:
+                    for key, value in self.entryTypes.items():
+                        for num, field in enumerate(self.entryTypes[key].fields):
+                            if field.name in constraintFields:
+                                print(constraintFields)
+                                self.entryTypes[key].fields[num].contraints = {
+                                    'dataype': dataType,
+                                    'rangemin': rangemin,
+                                    'rangemax': rangemax,
+                                    'pattern': pattern
+                                }
+                                #print(self.entryTypes[key].fields[num])
+            elif constraintType == 'conditional':
                 pass
 
-            if False:
-                value_type = reg_match.name_score.group(1)
-                line = next(file, None)
-                while line and line.strip():
-                    number, value = line.strip().split(',')
-                    value = value.strip()
-                    dict_of_data = {
-                        'School': school,
-                        'Grade': grade,
-                        'Student number': number,
-                        value_type: value
-                    }
-                    data.append(dict_of_data)
-                    line = next(file, None)
+class EntryType:
+    def __init__(self, name, skipout):
+        self.name = name
+        self.skipout = skipout
+        self.fields = []
 
-        return 
+    def __str__(self):
+        return 'name={}, skipout={}, fields={}'.format(self.name, self.skipout, [field.name for field in self.fields])
 
+class Field:
+    def __init__(self, name, type=False, dataType=False):
+        self.name = name
+        self.type = type
+        self.dataType = dataType
+        self.mandatory = False
+        self.orConstraint = False
+        self.xorConstraint = False
+        self.contraints = False
+
+    def __str__(self):
+        return 'name={}, type={}, dataType={}, mandatory={}, xorConstraint={}, orConstraint={}, contraints={}'.format(
+            self.name,
+            self.type,
+            self.dataType,
+            self.mandatory,
+            self.xorConstraint,
+            self.orConstraint,
+            self.contraints
+        )
 
 class _RegExLib:
     """Set up regular expressions"""
@@ -97,7 +202,7 @@ class _RegExLib:
         r'\\DeclareDatamodelEntrytypes(\[.*?\])?{(.*?)}', re.DOTALL | re.MULTILINE
     )
     _reg_fields = re.compile(
-        r'\\DeclareDatamodelFields(\[.*?\])?{(.*?)}', re.DOTALL | re.MULTILINE
+        r'\\DeclareDatamodelFields(\[.*?\]){(.*?)}', re.DOTALL | re.MULTILINE
     )
     _reg_entryFields = re.compile(
         r'\\DeclareDatamodelEntryfields(\[.*?\])?{(.*?)}', re.DOTALL | re.MULTILINE
@@ -105,6 +210,7 @@ class _RegExLib:
     _reg_multiscriptEntryFields = re.compile(
         r'\\DeclareDatamodelMultiscriptEntryfields(\[.*?\])?{(.*?)}', re.DOTALL | re.MULTILINE
     )
+    # TODO replace the '\n\n' part
     _reg_constraints = re.compile(
         r'\\DeclareDatamodelConstraints(\[.*?\])?{(.*?)}\n\n', re.DOTALL | re.MULTILINE
     )
